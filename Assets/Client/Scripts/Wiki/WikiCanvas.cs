36784567eae39b15618612ced4687705aabe97c8
DesertFoxHU <40893862+DesertFoxHU@users.noreply.github.com>
2022-08-25 01:52:04 +0200

    Wiki Pages
    [+] Added Wiki pages usability, you can see the previous and the next one
    [+] Added Buildings wiki page

--- Assets/Client/Scripts/Wiki/WikiCanvas.cs
+++ Assets/Client/Scripts/Wiki/WikiCanvas.cs
@@ -5,6 +5,31 @@ using UnityEngine;
 public class WikiCanvas : MonoBehaviour
 {
     public GameObject panel;
+    public List<OptionalConnectedPage> pages;
+    private OptionalConnectedPage? current;
+
+    private int index = 0;
+    public int pageIndex
+    {
+        get { return index; }
+        set
+        {
+            if(value < 0)
+            {
+                value = 0;
+            }
+            else if(value >= pages.Count)
+            {
+                value = pages.Count - 1;
+            }
+            index = value;
+        }
+    }
+
+    private void Start()
+    {
+        LoadCurrent();
+    }
 
     void Update()
     {
@@ -12,5 +37,41 @@ public class WikiCanvas : MonoBehaviour
         {
             panel.SetActive(!panel.activeSelf);
         }
+    }
+
+    public void LoadCurrent()
+    {
+        if(current != null)
+        {
+            current.GetValueOrDefault().SetActive(false);
+        }
+
+        current = pages[pageIndex];
+        current.GetValueOrDefault().SetActive(true);
+    }
+
+    public void Previous()
+    {
+        pageIndex -= 1;
+        LoadCurrent();
+    }
+
+    public void Next()
+    {
+        pageIndex += 1;
+        LoadCurrent();
+    }
+}
+
+[System.Serializable]
+public struct OptionalConnectedPage
+{
+    public WikiPage page1;
+    public WikiPage page2;
+
+    public void SetActive(bool isActive)
+    {
+        if(page1 != null) page1.gameObject.SetActive(isActive);
+        if(page2 != null) page2.gameObject.SetActive(isActive);
     }
 }
