6e41714fe890ef363d4ba405e350a6bef4156570
DesertFoxHU <40893862+DesertFoxHU@users.noreply.github.com>
2022-09-04 19:17:12 +0200

    Flag tile and spawning
    [+] Added flag tile
    [+] Flags are generate on the map with 40% chance and they never can be next to eachother.

--- Assets/Server/Scripts/Territory/ServerTerritory.cs
+++ Assets/Server/Scripts/Territory/ServerTerritory.cs
@@ -6,13 +6,21 @@ namespace ServerSide
 {
     public class ServerTerritory : Territory
     {
-        public AbstractBuilding building;
+        public AbstractBuilding building = null;
+        public Vector3Int Position;
         public Color Color { get; private set; }
 
         public ServerTerritory(AbstractBuilding building, ushort clientID, List<Vector3Int> claimedLand) : base(clientID, claimedLand)
         {
             this.building = building;
+            Position = building.Position;
             Color = NetworkManager.Find(clientID).Color;
+        }
+
+        public ServerTerritory(Vector3Int Position, ushort clientID, Color color, List<Vector3Int> claimedLand) : base(clientID, claimedLand)
+        {
+            this.Position = Position;
+            Color = color;
         }
 
         public override Color GetColor() => Color;
