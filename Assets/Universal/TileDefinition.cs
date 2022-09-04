71acba661de84173fdb8b22590e9d6a9541e8ea5
DesertFoxHU <40893862+DesertFoxHU@users.noreply.github.com>
2022-09-04 21:29:27 +0200

    .
    [+] Some changes, read changed files

--- Assets/Universal/TileDefinition.cs
+++ Assets/Universal/TileDefinition.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Tile/TileDefinition", fileName = "TileDefinition")]
-public class TileDefinition : ScriptableObject
+public class TileDefinition : RuleTile
 {
     public TileType type;
     public string tileName;
